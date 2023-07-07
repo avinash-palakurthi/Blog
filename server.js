e31c4c1ec56d76a72862a152d2d74a0561745653
Avinash <palakurthiavinash07@gmail.com>
2023-07-07 23:17:48 +0530

    server created

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const colors = require("colors");
+const dotenv = require("dotenv");
+
+//env config
+dotenv.config();
+
+//* if dotenv file is in another folder add path ex : {path:'/ folder name'}
+
+//rest object
+const app = express();
+
+//middlewares
+app.use(cors());
+app.use(express());
+app.use(morgan("dev"));
+
+//routes
+app.get("/", (req, res) => {
+  res.status(200).send({
+    message: "node server running",
+  });
+});
+
+//port
+const PORT = process.env.PORT || 8080;
+
+//listen
+app.listen(PORT, () => {
+  console.log(
+    `server running on ${process.env.DEV_MODE} port no ${PORT}`.bgCyan.white
+  );
+});
